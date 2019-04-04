ab7d5aed61e4be3a8408f6b95735b812dbaf3d24
Mitchell Johnson <ehntoo@gmail.com>
2019-04-04 02:48:47 -0400

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,42 @@ use hidapi::HidApi;
 pub enum FeatureReportOpcode {
     SetEffectType = 0x08,
     GetEffectType = 0x88,
-    SetPicture    = 0x12,
-    SetColor      = 0x14,
-    SetRowIndex   = 0x16,
+    SetPicture = 0x12,
+    SetColor = 0x14,
+    SetRowIndex = 0x16,
 }
 
 #[repr(u8)]
 pub enum EffectType {
-    FW      = 0,
-	Row     = 1,
-	Picture = 2,
-	Music   = 3,
-	AP      = 4,
+    FW = 0,
+    Row = 1,
+    Picture = 2,
+    Music = 3,
+    AP = 4,
 }
 
 #[repr(u8)]
 pub enum Effect {
-    Static    = 1,
-	Breathing = 2,
-	Reactive  = 4,
-	Wave      = 3,
-	Rainbow   = 5,
-	Ripple    = 6,
-	Nomo      = 8,
-	Marquee   = 9,
-	Raindrop  = 10,
-	Stack     = 12,
-	Impact    = 13,
-	Aurora    = 14,
-	Neon      = 15,
-	Spark     = 17,
-	Flash     = 18,
-	Mix       = 19,
-	Rippleo   = 22,
-	Music     = 34,
-	Usermode  = 51,
-	Unknown   = 255,
+    Static = 1,
+    Breathing = 2,
+    Reactive = 4,
+    Wave = 3,
+    Rainbow = 5,
+    Ripple = 6,
+    Nomo = 8,
+    Marquee = 9,
+    Raindrop = 10,
+    Stack = 12,
+    Impact = 13,
+    Aurora = 14,
+    Neon = 15,
+    Spark = 17,
+    Flash = 18,
+    Mix = 19,
+    Rippleo = 22,
+    Music = 34,
+    Usermode = 51,
+    Unknown = 255,
 }
 
 #[repr(u8)]
@@ -57,20 +57,21 @@ pub struct Color {
     b: u8,
 }
 
-pub fn _adjust_color(color: Color) -> Color {
+#[rustfmt::skip]
+fn _adjust_color(color: Color) -> Color {
     match color {
-        Color { r: 255, g: 255, b: 255} => Color { r: 255, g: 180, b: 200},
-        Color { r: 243, g: 152, b: 0} => Color { r: 255, g: 42, b: 0},
-        Color { r: 241, g: 90, b: 36} => Color { r: 255, g: 25, b: 0},
-        Color { r: 247, g: 147, b: 30} => Color { r: 255, g: 42, b: 0},
-        Color { r: 255, g: 241, b: 0} => Color { r: 255, g: 180, b: 0},
-        Color { r: 0, g: 255, b: 255} => Color { r: 0, g: 180, b: 200},
-        Color { r: 138, g: 0, b: 255} => Color { r: 138, g: 0, b: 200},
-        Color { r: r@250...255, g: g@0...19, b: b@0...99} => Color { r, g, b: b/10},
-        Color { r: r@250...255, g: g@0...19, b: b@100...127} => Color { r, g, b: b-10},
-        Color { r: r@0...19, g, b} if b < g => Color { r: r, g, b: 200*b/255 },
-        Color { r, g: g@0...19, b} if r < b => Color { r, g, b },
-        Color { r, g, b } => Color { r, g: 180*g/255, b: 200*b/255},
+        Color {r: 255, g: 255, b: 255} => Color {r: 255, g: 180, b: 200},
+        Color {r: 243, g: 152, b: 0} => Color {r: 255, g: 42, b: 0},
+        Color {r: 241, g: 90, b: 36} => Color {r: 255, g: 25, b: 0},
+        Color {r: 247, g: 147, b: 30} => Color {r: 255, g: 42, b: 0},
+        Color {r: 255, g: 241, b: 0} => Color {r: 255, g: 180, b: 0},
+        Color {r: 0, g: 255, b: 255} => Color {r: 0, g: 180, b: 200},
+        Color {r: 138, g: 0, b: 255} => Color {r: 138, g: 0, b: 200},
+        Color {r: r@250...255, g: g@0...19, b: b@0...99} => Color {r, g, b: b/10},
+        Color {r: r@250...255, g: g@0...19, b: b@100...127} => Color {r, g, b: b-10},
+        Color {r: r@0...19, g, b} if b < g => Color {r: r, g, b: 200*b/255},
+        Color {r, g: g@0...19, b} if r < b => Color {r, g, b },
+        Color {r, g, b} => Color {r, g: 180*g/255, b: 200*b/255},
     }
 }
 
@@ -79,8 +80,19 @@ fn main() {
 
     let kb = api.open(1165, 52736).expect("Failed to open device");
 
-    let disable_report = [0u8, FeatureReportOpcode::SetEffectType as u8, ControlLED::Off as u8, 0, 0, 0, 0, 0, 0];
-    kb.send_feature_report(&disable_report).expect("Failed to send feature report");
+    let disable_report = [
+        0u8,
+        FeatureReportOpcode::SetEffectType as u8,
+        ControlLED::Off as u8,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+    ];
+    kb.send_feature_report(&disable_report)
+        .expect("Failed to send feature report");
 
     // std::thread::sleep(std::time::Duration::from_secs(1));
 
@@ -94,25 +106,38 @@ fn main() {
         brightness,
         0, // colorindex
         0, // direction
-        0  // save
+        0, // save
     ];
-    kb.send_feature_report(&refresh_report).expect("Failed to send refresh report");
+    kb.send_feature_report(&refresh_report)
+        .expect("Failed to send refresh report");
 
-    let set_picture_report = [0u8, FeatureReportOpcode::SetPicture as u8, 0, 0, 8, 0, 0, 0, 0];
-    kb.send_feature_report(&set_picture_report).expect("Failed to send set picture report");
+    let set_picture_report = [
+        0u8,
+        FeatureReportOpcode::SetPicture as u8,
+        0,
+        0,
+        8,
+        0,
+        0,
+        0,
+        0,
+    ];
+    kb.send_feature_report(&set_picture_report)
+        .expect("Failed to send set picture report");
 
-    let display_color = Color { r: 0xff, g: 0x2a, b: 0 };
+    let display_color = Color {
+        r: 0xff,
+        g: 0x2a,
+        b: 0,
+    };
     // let color = Color { r: 255, g: 255, b: 255 };
     // let display_color = _adjust_color(color);
 
     let mut key_array = [0u8; 65];
     for i in (1..65).step_by(4) {
-        key_array[i+1] = display_color.r;
-        key_array[i+2] = display_color.g;
-        key_array[i+3] = display_color.b;
-        // key_array[i+1] = 0xff; R
-        // key_array[i+2] = 0xff; G
-        // key_array[i+3] = 0xff; B
+        key_array[i + 1] = display_color.r;
+        key_array[i + 2] = display_color.g;
+        key_array[i + 3] = display_color.b;
     }
     for _ in 0..8 {
         kb.write(&key_array).expect("Failed to write key data");
